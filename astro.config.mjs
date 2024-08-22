f2d501f24a780fbfac137ec7119f1ef093dc1ab9
taoguangc <taoguangc@gmail.com>
2024-08-22 09:03:32 +0800

    remove @astrojs/cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config'
-import cloudflare from '@astrojs/cloudflare'
 import partytown from '@astrojs/partytown'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
@@ -8,7 +7,6 @@ import sitemap from '@astrojs/sitemap'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-rokvi.pages.dev/',
-  adapter: cloudflare(),
   integrations: [
     tailwind(),
     icon(),
